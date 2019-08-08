334d2f248e380a958b2973762708e5416436c813
StruggleYang <1724555319@qq.com>
2019-08-08 23:23:50 +0800

    ss

--- yum_install_docker_ce.sh
+++ yum_install_docker_ce.sh
@@ -81,8 +81,8 @@ yum-config-manager --enable docker-ce-edge
 yum install docker-ce -y
 
 # install docker-compose
-chmod +x /usr/local/bin/docker-compose
-curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+#curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+curl -L https://get.daocloud.io/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
 rm -rf /etc/yum.repos.d/docker-ce.repo
